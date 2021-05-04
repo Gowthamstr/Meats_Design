70bd8a53951f90bbc7edb6f0b18169320669dc3b
Gowthamstr <83452992+Gowthamstr@users.noreply.github.com>
2021-05-04 12:12:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function showandhide(){
+    var x = document.getElementById('Flex');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function getdata(){
+    var x = document.getElementById('Flex1');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function getdetail(){
+    var x = document.getElementById('Flex2');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
